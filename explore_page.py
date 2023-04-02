da517d89ed152ebdcddafc6073f51c2f97076c20
daguang123456 <poiuylkjhgmnbvc12@outlook.com>
2023-04-02 09:01:01 -0700

    update

--- explore_page.py
+++ explore_page.py
@@ -33,6 +33,7 @@ def clean_education(x):
 @st.cache
 def load_data():
     df = pd.read_csv("./survey_results_public.csv")
+    print(df.head())
     df = df[["Country", "EdLevel", "YearsCodePro", "Employment", "ConvertedCompYearly"]]
     df = df[df["ConvertedCompYearly"].notnull()]
     df = df.dropna()
@@ -62,20 +63,27 @@ def show_explore_page():
     )
 
     data = df["Country"].value_counts()
+    print(data)
+    print(df.head())
 
+    fig1, ax1 = plt.subplots()
+    ax1.pie(data, labels=data.index, autopct="%1.1f%%", shadow=True, startangle=90)
+    ax1.axis("equal")  # Equal aspect ratio ensures that pie is drawn as a circle.
+    st.write("""#### 来自不同国家的数据数量""")
+    st.pyplot(fig1)
+
+    # Pie chart, where the slices will be ordered and plotted counter-clockwise:
+    # labels = 'Frogs', 'Hogs', 'Dogs', 'Logs'
+    # sizes = [15, 30, 45, 10]
+    # explode = (0, 0.1, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
 
     # fig1, ax1 = plt.subplots()
-    # ax1.pie(data, labels=data.index, autopct="%1.1f%%", shadow=True, startangle=90)
-    # ax1.axis("equal")  # Equal aspect ratio ensures that pie is drawn as a circle.
-
-    # st.write("""#### 来自不同国家的数据数量""")
+    # ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+    #         shadow=True, startangle=90)
+    # ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 
     # st.pyplot(fig1)
-    fig = plt.figure()
-    plt.pie(data, labels=data.index, autopct="%1.1f%%", shadow=True, startangle=90)
 
-
-    st.pyplot(fig)
     
     # st.write(
     #     """
